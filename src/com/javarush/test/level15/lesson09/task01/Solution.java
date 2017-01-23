df9b08ae303ca4dae2082257bcc333225bcf549f
Gennady Bal <balgena2@gmail.com>
2017-01-23 12:28:12 +0200

    Решение 2

--- src/com/javarush/test/level15/lesson09/task01/Solution.java
+++ src/com/javarush/test/level15/lesson09/task01/Solution.java
@@ -10,6 +10,14 @@ import java.util.Map;
 public class Solution {
     public static Map<Double, String> labels = new HashMap<Double, String>();
 
+    static {
+        labels.put((double)1,"1");
+        labels.put((double)2,"2");
+        labels.put((double)3,"3");
+        labels.put((double)4,"4");
+        labels.put((double)5,"5");
+    }
+
     public static void main(String[] args) {
         System.out.println(labels);
     }
