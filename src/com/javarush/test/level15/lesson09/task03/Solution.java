39e38d361d14be4eec87486d8cc8386acfe9d898
Gennady Bal <balgena2@gmail.com>
2017-01-24 23:29:36 +0200

    15.lesson09.task03 decided

--- src/com/javarush/test/level15/lesson09/task03/Solution.java
+++ src/com/javarush/test/level15/lesson09/task03/Solution.java
@@ -13,6 +13,23 @@ StringVar с типом String
 */
 
 public class Solution {
+
+    static public int intVar;
+    static public double doubleVar;
+    static public Double DoubleVar;
+    static public boolean booleanVar;
+    static public Object ObjectVar;
+    static public Exception ExceptionVar;
+    static public String StringVar;
+
     public static void main(String[] args) {
+
+        System.out.println(intVar);
+        System.out.println(doubleVar);
+        System.out.println(DoubleVar);
+        System.out.println(booleanVar);
+        System.out.println(ObjectVar);
+        System.out.println(ExceptionVar);
+        System.out.println(StringVar);
     }
 }
