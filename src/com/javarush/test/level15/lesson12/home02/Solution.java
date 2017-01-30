51e7d10a5396c00b2b87bd977104865e48c3931d
Gennady Bal <balgena2@gmail.com>
2017-01-30 19:02:40 +0200

    level15.lesson12.home02 решена

--- src/com/javarush/test/level15/lesson12/home02/Solution.java
+++ src/com/javarush/test/level15/lesson12/home02/Solution.java
@@ -20,6 +20,11 @@ public class Solution {
         public void doAction() {
             System.out.println("flying");
         }
+
+        @Override
+        public void doAction2() {
+            System.out.println("moving");
+        }
     }
 
     public static class Util {
@@ -28,7 +33,7 @@ public class Solution {
         }
 
         static void move(Movable animal) {
-            animal.doAction();
+            animal.doAction2();
         }
     }
 
@@ -37,6 +42,6 @@ public class Solution {
     }
 
     public static interface Movable {
-        void doAction();
+        void doAction2();
     }
 }
